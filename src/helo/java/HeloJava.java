a107278f5c243b837845af9be1602d40d254d099
zahravaniaa <zahraivaniaa@gmail.com>
2022-01-12 13:16:41 +0700

    First Commit

---
+++ src/helo/java/HeloJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helo.java;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class HeloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Halo ini vania dan tita");
+    }
+    
+}
