677ae31aa87c4765534af224d7e0ecb0dc4c92d4
zahravaniaa <97347348+zahravaniaa@users.noreply.github.com>
2022-01-12 13:20:23 +0700

    Update HeloJava.java

--- src/helo/java/HeloJava.java
+++ src/helo/java/HeloJava.java
@@ -16,6 +16,7 @@ public class HeloJava {
      */
     public static void main(String[] args) {
         System.out.println("Halo ini vania dan tita");
+        System.out.println("Selamat datang di projek kami");
     }
     
 }
